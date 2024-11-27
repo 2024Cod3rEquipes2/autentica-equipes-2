3fbc13bbf7063590d5f4c6646bc65728ece7f8bc
Danilo <silvacdanilo1910@gmail.com>
2024-11-26 22:07:33 -0300

    Adicionado pasta backend com a funcionalidade de login e um regitro basico

---
+++ apps/backend/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'entities/user.entity';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
