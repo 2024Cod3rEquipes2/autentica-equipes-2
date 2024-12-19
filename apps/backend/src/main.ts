529ed925e7659e9a435340633da2eb39ea7664d6
Allan Farias <allan.digitaltec@gmail.com>
2024-12-19 19:51:55 -0300

    Add permissões cors e db ao gitignore

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: false, // Remove propriedades não permitidas
-      forbidNonWhitelisted: false, // Lança um erro se propriedades não permitidas forem enviadas
-      transform: true, // Transforma os objetos de entrada em instâncias de classe
-    }),
-  );
-  await app.listen(4000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: false, // Remove propriedades não permitidas
+			forbidNonWhitelisted: false, // Lança um erro se propriedades não permitidas forem enviadas
+			transform: true, // Transforma os objetos de entrada em instâncias de classe
+		}),
+	);
+	await app.listen(4000);
 }
 bootstrap();
