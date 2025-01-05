5ab372b62667f2a537c88be98beaa77e8d925623
Sbnc <sabriinacorrea13@hotmail.com>
2025-01-05 18:28:54 -0300

    Ajustando uns detalhes no layout

--- apps/frontend/src/app/components/auth/LoginForm.tsx
+++ apps/frontend/src/app/components/auth/LoginForm.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import {
-  EnvelopeIcon,
-  EyeIcon
-} from "@heroicons/react/24/outline";
+import { EnvelopeIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Button from "../shared/Button/Button";
 import Input from "../shared/Input/Input";
 import Titulo from "../shared/Titulo/Titulo";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-
 
 const usuarios = [
   {
@@ -42,7 +38,6 @@ export default function LoginForm() {
     verificarEmail(email, senha);
   };
 
-
   const alterarEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -51,13 +46,13 @@ export default function LoginForm() {
     setSenha(e.target.value);
   };
 
-    function verificarEmail(email: string, senha: string): void {
+  function verificarEmail(email: string, senha: string): void {
     const usuario = usuarios.find((usuario) => usuario.email === email);
 
     if (usuario && usuario.senha === senha) {
       alert(`Bem-vindo, ${usuario.nome}!`);
     }
-      alert("Email ou senha inválidos.");
+    alert("Email ou senha inválidos.");
   }
 
   return (
@@ -66,36 +61,36 @@ export default function LoginForm() {
 
       <Titulo texto="Entre com sua conta" className="text-2xl" />
 
-      <form onSubmit={enviarFormularioLogin}>
-      <div className="flex flex-col w-full">
-        <Input
-          label="Email"
-          IconeLadoDireito={EnvelopeIcon}
-          tipo="email"
-          tamanho={6}
-          onChange={alterarEmail}
-          value={email}
-        />
+      <form onSubmit={enviarFormularioLogin} className="flex flex-col w-full">
+        <div>
+          <Input
+            label="Email"
+            IconeLadoDireito={EnvelopeIcon}
+            tipo="email"
+            tamanho={6}
+            onChange={alterarEmail}
+            value={email}
+          />
 
-        <Input
-          label="Senha"
-          IconeLadoDireito={EyeIcon}
-          tipo="password"
-          tamanho={6}
-          onChange={alterarSenha}
-          value={senha}
-        />
-      </div>
+          <Input
+            label="Senha"
+            IconeLadoDireito={EyeIcon}
+            tipo="password"
+            tamanho={6}
+            onChange={alterarSenha}
+            value={senha}
+          />
+        </div>
 
-      <Link
-        href={"/recuperacao-senha"}
-        className="self-end text-textoCinza text-md pb-4 hover:brightness-125 transition"
-      >
-        Esqueceu a senha?
-      </Link>
-      <Button className="verde" tipo="submit" >
-        Login
-      </Button>
+        <Link
+          href={"/recuperacao-senha"}
+          className="self-end text-textoCinza text-md pb-4 hover:brightness-125 transition"
+        >
+          Esqueceu a senha?
+        </Link>
+        <Button className="verde" tipo="submit">
+          Login
+        </Button>
       </form>
       <div className="flex items-center justify-center">
         <hr className="linha my-8" />{" "}
@@ -111,7 +106,7 @@ export default function LoginForm() {
           height={60}
         />
       </button>
-      
+
       <hr />
       <span className="text-center text-textoBranco text-lg leading-5">
         Ainda não possui uma conta?{" "}
